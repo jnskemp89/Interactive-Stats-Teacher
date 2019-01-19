0c5b6d8f2938f6c91223c707d2918a7479f5a44b
jnskemp89 <jnskemp89@gmail.com>
2019-01-19 03:44:58 -0600

    text complete

--- server.R
+++ server.R
@@ -16,7 +16,8 @@ shinyServer(function(input, output) {
     
     # draw the histogram 
     x <-  seq(min(Galton$childHeight), max(Galton$childHeight), length.out = 934)
-    df <- data.frame(height = x, density = dnorm(x, input$mean3, input$sd3), cumulative = rnorm(x, input$mean3, input$sd3))
+    df <- data.frame(height = x, density = dnorm(x, input$mean, input$sd), 
+                     cumulative = rnorm(x, input$mean, input$sd))
     
     ggplot(Galton) +
       geom_histogram(aes(x = childHeight, y = ..density..),
@@ -30,7 +31,9 @@ shinyServer(function(input, output) {
     
     # draw the histogram 
     x <-  seq(min(Galton$childHeight), max(Galton$childHeight), length.out = 934)
-    df <- data.frame(height = x, density = dnorm(x, input$mean3, input$sd3), cumulative = rnorm(x, input$mean3, input$sd3))
+    x1 <- rnorm(934, input$mean2, input$sd2)
+    df <- data.frame(height = x1, density = dnorm(x1, input$mean2, input$sd2), 
+                     cumulative = rnorm(934, input$mean2, input$sd2))
     
     plot1 <- ggplot(Galton) +
       geom_histogram(aes(x = childHeight, y = ..density..),
@@ -38,8 +41,10 @@ shinyServer(function(input, output) {
       geom_line(data = df, aes(x = height, y = density), color = "red")
       
     plot2 <- ggplot(df) + 
-      geom_boxplot(aes(y = density, alpha = 0.7, fill = "red")) +
-      theme(legend.position = "none")
+      geom_boxplot(aes(y = x1, alpha = 0.7, fill = "red")) +
+      theme(legend.position = "none") + 
+      ylim(55, 80) + 
+      coord_flip()
     
     grid.arrange(plot1, plot2, ncol=2)
     
@@ -50,7 +55,8 @@ shinyServer(function(input, output) {
     
     # draw the histogram 
     x <-  seq(min(Galton$childHeight), max(Galton$childHeight), length.out = 934)
-    df <- data.frame(height = x, density = dnorm(x, input$mean3, input$sd3), cumulative = rnorm(x, input$mean3, input$sd3))
+    df <- data.frame(height = x, density = dnorm(x, input$mean3, input$sd3), 
+                     cumulative = rnorm(x, input$mean3, input$sd3))
     
     plot1 <- ggplot(Galton) +
       geom_histogram(aes(x = childHeight, y = ..density..),
@@ -58,10 +64,18 @@ shinyServer(function(input, output) {
       geom_line(data = df, aes(x = height, y = density), color = "red")
     #geom_boxplot(y = df$y, horiz = TRUE)
     plot2 <- ggplot(df, aes(cumulative)) + 
-      xlim(min(Galton$childHeight), max(Galton$childHeight)) +
+      xlim(50, 85) +
       stat_ecdf()
     grid.arrange(plot1, plot2, ncol=2)
     
     
   })
+  
+  output$boxPlot <- renderPlot({
+    set.seed(40)
+    X <- rnorm(100, 50, 25)
+    boxplot(X, horizontal = TRUE, axes = FALSE, staplewex = 1)
+    text(x=fivenum(X), labels =c("min", "Q1", "Q2", "Q3", "max"), y=1.35)
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -37,19 +37,52 @@ ui <- dashboardPage(skin = "red",
                 interactive graphs so that the effects of these statistics are easily displayed
                 and understood."),
                 br(),
-                h4("As this is must be a somewhat brief format textbook to fit the constrictions
+                h4("As this must be a somewhat brief format textbook to fit the constrictions
                 of this app, we will provide a brief glossary of some of the more important 
-                concepts while assuming some basic understanding of the underlying concepts."), 
+                concepts, written in ", strong('bold'), ", while assuming some basic understanding of the underlying concepts.
+                The intent will be to show in particular how specifically the population variables
+                of mean and standard deviation link and influence the three chosen topics."), 
                 width = 12
               )#box
       ),#tabItem
       tabItem(tabName = "ndistExp",
-              h3("Normal Distributions"),
-              br(),
-              h4("Explanation of mean and std in relation to normal distribution")
+              box(
+                h3("Normal Distributions"),
+                br(),
+                h4('The normal distribution is the most important and most widely used 
+                  distribution in statistics. It is sometimes called the "bell curve," 
+                  or the "Gaussian curve" after the mathematician Karl Friedrich Gauss. 
+                  These distributions are generally indicative of a dataset containing 
+                  highly independent variables, meaning that the observed findings do 
+                  not have any underlying factors influencing the variable in question.'),
+                br(),
+                h4('All normal distributions are symmetric with relatively 
+                  more values at the center of the distribution and relatively few in 
+                  their tails. Where normal distributions can differ is in their means and 
+                  in their standard deviations. The ', strong('mean'), ' , as the average value of a dataset,
+                  will be the value on the x-axis where the majority of the data points
+                  are clustered. If the mean changes, then the center of the distribution 
+                  changes. The ', strong('standard deviation'), ' of the data tells us how far on average 
+                  the data will stray from the mean to either the positive or the negative 
+                  side. Therefore, the smaller the deviation is, the more narrow the 
+                  distribution will be, and a larger deviation will result in a wider range 
+                  to the distribution.'),
+                br(),
+                h4("Now take a look at the demonstration tab so see how changing these two 
+                  values will affect the distribution as a whole. By default for this and all
+                  other demonstrations in this application, the mean and standard deviation 
+                  will be set to match the values in Galton's height data, depicted by a 
+                  static histogram to give a point of reference."),
+                br(),
+                h5(a("Reference - OnlineStatBook", href = "http://onlinestatbook.com/2/normal_distribution/intro.html")),
+                width = 12
+              )#box
       ),#tabItem
       tabItem(tabName = "ndistDemo",
-              h3("Use the sliders below to visualize the impact that mean and standard deviation have on a distribution"),
+              box(h4("Use the sliders below to visualize the impact that mean and standard 
+                 deviation have on a distribution. The default red line matches the 
+                 underlying histogram of Galton's data, and will change according to 
+                 the sliders. The histogram will remain static to give a point of reference."), width = 12),
               fluidRow(
                 box(
                   sliderInput("mean",
@@ -71,7 +104,37 @@ ui <- dashboardPage(skin = "red",
               plotOutput("distPlot")
       ),#tabItem
       tabItem(tabName = "quartExp",
-              h3("Explanation of interquartile ranges")
+              fluidRow(
+                box(
+                h3("Interquartile Ranges (IQRs)"),
+                br(),
+                h4('To understand the interquartile range, it would be best to start with 
+                   an explanation of how it is created. We first order all points of data 
+                  in order from least to greatest. Then we must determine three specific 
+                  data points:'),
+                tags$div(tags$ul(
+                  tags$li(h4("The ", strong('median'), ", which will be labelled Q2")),
+                  tags$li(h4("The median of the half of the data which precedes Q2, which will be labelled Q1")),
+                  tags$li(h4("The median of the half of the data which follows Q2, which will be labelled Q3")))),
+                h4('These points will be used to create the boundaries of our IQR, sectioning
+                  off the dataset into four equally sized parts. The IQR is depicted by 
+                  drawing a box with sides at the Q1 and Q3 boundaries, with Q2 running 
+                  through the middle, and a line emerging perpendicular from either side 
+                  and ending at the minimun and maximum values of the dataset. Any points that 
+                  are more than two standard deviations away from the mean are labelled outliers 
+                  and marked by a dot on the graph.'),
+              
+                plotOutput("boxPlot"),
+              
+                h4("It is easy to see how the mean will affect this particular chart, since 
+                  it the mean and the median values of a set are so closely related. Changing 
+                  the mean will by rule change the Q2 value in a very similar way. Increasing standard 
+                  deviation will cause the data to vary further from the median, thus the widths of the 
+                  four quartiles will increase. Take a look at the demonstration on the next page to see
+                  these effects in action. The histogram and bell curve from the previous example are 
+                  provided here as well for context."), width = 12
+              )#box
+              )#fluidRow
       ),#tabItem
       tabItem(tabName = "quartDemo",
               h3("Use the sliders below to visualize the impact that mean and standard deviation have on interquartile ranges."),
@@ -96,7 +159,52 @@ ui <- dashboardPage(skin = "red",
               plotOutput("quartPlot")
       ),#tabItem
       tabItem(tabName = "cdfExp",
-              h3("Explanation of CDF")
+              fluidRow(
+                box(
+                  h3("Cumulative Distributuion Function (CDF)"),
+                  br(),
+                  h4("The CDF is a function which shows how quickly a data set reaches
+                     100% of its data points over the observed data range. From observing 
+                     the histogram of the Galton dataset we can see that all of our 
+                     height data falls between 56 and 79 inches. This means that the 
+                     CDF will increase from zero to one in that same interval."),
+                  br(),
+                  h4("The rate at which the function increases it dependent on the 
+                     accumulation of data points at each value within this range. This 
+                     means that, for a data set with 100 points of data, if the first 
+                     position in the range holds 3 identical data points, then the CDF 
+                     will increase by 0.03 at that position (3%). This continues for 
+                     every position in the range of the data values until all the data 
+                     has been accounted for."),
+                  br(),
+                  h4('The CDF of a normally distributed variable will always take on a 
+                     distinctive "S" shape due to the bell-shaped distribution. Because
+                     the tails contain fewer data points being furthest away from the 
+                     mean, they will have the slowest step-by-step increase contributing
+                     to the CDF. The range values at and near the mean, where most of 
+                     the data is clustered, will give the sharpest increase. Then on the
+                     other side of the mean, as the number of values starts to decrease 
+                     again, the slope of the corresponding CDF will begin to level off 
+                     until it reaches the end of the range. Another way to think about this 
+                     is by taking the histogram bar at each position in the range and 
+                     stacking it on top of all the previous histogram bars. The line 
+                     connecting the tops of all of these stacks will be our CDF.'),
+                  br(),
+                  h4("This means that the mean and standard deviation of the normal 
+                     distribution will have an intuitive impact on the shape of this 
+                     function. A change in the mean will result in a change in the 
+                     position where we have the sharpest increase in the graph, which 
+                     is to say that the center of our 'S' shape will slide to match the 
+                     mean. We already know that an increase to the standard deviation 
+                     results in a data set that is more evenly spread out across the 
+                     range of the data, making our histogram more level and the increases 
+                     across our CDF more gradual. Of course the opposite also holds true - 
+                     a smaller standard deviation results in data which is more tightly 
+                     condensed around the mean, which will make for a much steeper and 
+                     sudden increase in the CDF."),
+                  width = 12
+                )#box
+              )#fluidRow
       ),#tabItem
       tabItem(tabName = "cdfDemo",
               h3("Use the sliders below to visualize the impact that mean and standard
@@ -122,7 +230,14 @@ ui <- dashboardPage(skin = "red",
               plotOutput("cdfPlot")
       ),#tabItem
       tabItem(tabName = "Glossary",
-              h3("List of important terms")
+              fluidRow(
+                box(
+                  h4(strong("Mean - "), "The arithmatic average of the dataset."),
+                  h4(strong("Standard Deviation - "),"A measure of the variation or dispersion of a dataset. A low standard deviation indicates that the majority of the data is close to the mean, and a large standard deviation means that the data is spread out over a wider range of values."),
+                  h4(strong("Median - "), "In an ordered dataset, this is the middle point separating the lower and upper halves of the data. This is the actual central data point, rather than a calculated average."),
+                  h4(""), width = 12
+                )#box
+              )#fluidRow
       )#tabItem
     )#tabItems        
   )#dashboardBody
